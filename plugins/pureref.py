8523c76d1e6a81283940187655bf2b88abb0ac70
ddesmond <crni.aki@gmail.com>
2024-07-28 06:51:26 +0200

    pureref, pylint

--- plugins/pureref.py
+++ plugins/pureref.py
@@ -16,20 +16,27 @@ class PureRefInstaller(Plugin):
     """
 
     _alias_ = "PureRef Installer"
+    # pylint: disable=line-too-long
     icon = "https://github.com/juno-fx/Terra-Official-Plugins/blob/main/plugins/assets/pureref.png?raw=true"
     description = "Install PureRef to a target directory."
     category = "Media and Entertainment"
     tags = ["PureRef", "references", "media", "vfx", "visual effects", "images"]
     fields = [
-        Plugin.field("version", "Version of PureRef to install. i.e. PureRef Latest", required=True),
+        Plugin.field(
+            "version",
+            "Version of PureRef to install. i.e. PureRef Latest",
+            required=True,
+        ),
         Plugin.field("destination", "Destination directory", required=True),
     ]
 
+    # pylint: disable=unused-argument
     def preflight(self, *args, **kwargs) -> bool:
         """
         Check if the target directory exists and validate the arguments passed.
         """
         # store on instance
+        # pylint: disable=attribute-defined-outside-init
         self.version = kwargs.get("version")
         self.destination = kwargs.get("destination")
 
@@ -49,11 +56,11 @@ class PureRefInstaller(Plugin):
         scripts_directory = os.path.abspath(f"{__file__}/../scripts")
         self.logger.info(f"Loading scripts from {scripts_directory}")
         if (
-                run(
-                    f"bash {scripts_directory}/PureRef-installer.sh {self.version} {self.destination}",
-                    shell=True,
-                    check=False,
-                ).returncode
-                != 0
+            run(
+                f"bash {scripts_directory}/PureRef-installer.sh {self.version} {self.destination}",
+                shell=True,
+                check=False,
+            ).returncode
+            != 0
         ):
             raise RuntimeError("Failed to install PureRef")
