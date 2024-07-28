ad7b53442d84549b5117242130897a4c1bc31177
ddesmond <crni.aki@gmail.com>
2024-07-28 04:43:30 +0200

    init commit

---
+++ plugins/mrv2.py
@@ -0,0 +1,59 @@
+"""
+Handles installing Mrv2
+"""
+
+# std
+import os
+from subprocess import run
+
+# 3rd
+from terra import Plugin
+
+
+class Mrv2Installer(Plugin):
+    """
+    Mrv2
+    """
+
+    _alias_ = "Mrv2 Installer"
+    icon = "https://github.com/ggarra13/mrv2/blob/main/mrv2/icons/mrviewer.svg"
+    description = "Install Mrv2 to a target directory."
+    category = "Media and Entertainment"
+    tags = ["mrv2", "viewer", "media", "vfx", "visual effects", "video", "sequences", "exr"]
+    fields = [
+        Plugin.field("version", "Version of Mrv2 to install.", required=True),
+        Plugin.field("destination", "Destination directory", required=True),
+    ]
+
+    def preflight(self, *args, **kwargs) -> bool:
+        """
+        Check if the target directory exists and validate the arguments passed.
+        """
+        # store on instance
+        self.version = kwargs.get("version")
+        self.destination = kwargs.get("destination")
+
+        # validate
+        if not self.destination:
+            raise ValueError("No destination directory provided")
+
+        if not self.destination.endswith("/"):
+            self.destination += "/"
+
+        os.makedirs(self.destination, exist_ok=True)
+
+    def install(self, *args, **kwargs) -> None:
+        """
+        Download and unpack the Mrv2.
+        """
+        scripts_directory = os.path.abspath(f"{__file__}/../scripts")
+        self.logger.info(f"Loading scripts from {scripts_directory}")
+        if (
+            run(
+                f"bash {scripts_directory}/mrv2-installer.sh {self.version} {self.destination}",
+                shell=True,
+                check=False,
+            ).returncode
+            != 0
+        ):
+            raise RuntimeError("Failed to install Mrv2")
