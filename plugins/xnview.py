50bb5b19029985c3a56b00fb04dfea0869c5fa9e
ddesmond <crni.aki@gmail.com>
2024-07-30 17:32:28 +0200

    Add Xnview to Terra

---
+++ plugins/xnview.py
@@ -0,0 +1,63 @@
+"""
+Installer for Xnview on linux systems.
+"""
+
+# std
+import os
+from subprocess import run
+
+# 3rd
+from terra import Plugin
+
+
+class XnviewInstaller(Plugin):
+    """
+    Xnview installer plugin.
+    """
+    _alias_ = "Xnview Installer"
+    icon = (
+        "https://www.xnview.com/img/app-xnviewmp-512.webp"
+    )
+    description = "XnView MP is a versatile and powerful photo viewer, image management, image resizer."
+    category = "Media and Entertainment"
+    tags = ["Xnview", "editor", "media", "editorial", "kde"]
+    fields = [
+        Plugin.field("url", "Download URL", required=False),
+        Plugin.field("destination", "Destination directory", required=True),
+    ]
+
+    def preflight(self, *args, **kwargs) -> bool:
+        """
+        Check if the target directory exists and validate the arguments passed.
+        """
+        # store on instance
+        self.download_url = kwargs.get(
+            "url",
+            "https://download.xnview.com/XnView_MP.glibc2.17-x86_64.AppImage",
+        )
+        self.destination = kwargs.get("destination")
+
+        # validate
+        if not self.destination:
+            raise ValueError("No destination directory provided")
+
+        if not self.destination.endswith("/"):
+            self.destination += "/"
+
+        os.makedirs(self.destination, exist_ok=True)
+
+    def install(self, *args, **kwargs) -> None:
+        """
+        Download and unpack the appimage to the destination directory.
+        """
+        scripts_directory = os.path.abspath(f"{__file__}/../scripts")
+        self.logger.info(f"Loading scripts from {scripts_directory}")
+        if (
+                run(
+                    f"bash {scripts_directory}/xnview-installer.sh {self.download_url} {self.destination}",
+                    shell=True,
+                    check=False,
+                ).returncode
+                != 0
+        ):
+            raise RuntimeError("Failed to install xnview")
