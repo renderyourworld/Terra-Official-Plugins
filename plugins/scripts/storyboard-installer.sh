b8388168a9d3268e68563c0fa7454a5d3fb971dc
ddesmond <crni.aki@gmail.com>
2024-07-30 18:52:43 +0200

    Add Storyboard to Terra

---
+++ plugins/scripts/storyboard-installer.sh
@@ -0,0 +1,7 @@
+wget -q -O /tmp/storyboard.appimage "$1"
+chmod +x /tmp/storyboard.appimage
+/tmp/storyboard.appimage --appimage-extract > /dev/null
+mv ./squashfs-root "$2/"
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
