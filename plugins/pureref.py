daf831aee8d6c14a4195119eefb44d95c88da5f7
ddesmond <crni.aki@gmail.com>
2024-07-30 06:12:05 +0200

    PureRef install

--- plugins/pureref.py
+++ plugins/pureref.py
@@ -6,16 +6,48 @@ PureRef - Simple and lightweight reference image viewer.
 # std
 import os
 from subprocess import run
+import requests
 
 # 3rd
 from terra import Plugin
+
+
+
+def download_file_from_google_drive(id, destination):
+    def get_confirm_token(response):
+        for key, value in response.cookies.items():
+            if key.startswith('download_warning'):
+                return value
+
+        return None
+
+    def save_response_content(response, destination):
+        CHUNK_SIZE = 32768
+
+        with open(destination, "wb") as f:
+            for chunk in response.iter_content(CHUNK_SIZE):
+                if chunk: # filter out keep-alive new chunks
+                    f.write(chunk)
+
+    URL = "https://docs.google.com/uc?export=download"
+
+    session = requests.Session()
+
+    response = session.get(URL, params = { 'id' : id }, stream = True)
+    token = get_confirm_token(response)
+
+    if token:
+        params = { 'id' : id, 'confirm' : token }
+        response = session.get(URL, params = params, stream = True)
+
+    save_response_content(response, destination)
+
 
 
 class PureRefInstaller(Plugin):
     """
     PureRef
     """
-
     _alias_ = "PureRef Installer"
     # pylint: disable=line-too-long
     icon = "https://github.com/juno-fx/Terra-Official-Plugins/blob/main/plugins/assets/pureref.png?raw=true"
@@ -56,12 +88,23 @@ class PureRefInstaller(Plugin):
         """
         scripts_directory = os.path.abspath(f"{__file__}/../scripts")
         self.logger.info(f"Loading scripts from {scripts_directory}")
-        if (
+
+        # TAKE ID FROM SHAREABLE LINK
+        file_id = "1FBh5-xB7tZnrK6JuzjcKY5Fx7PVQf4pK"
+        # DESTINATION FILE ON YOUR DISK
+        file_destination = self.destination + "/pureref2.Appimage"
+        download_file_from_google_drive(file_id, file_destination)
+
+        if os.path.exists(destination):
+            if (
                 run(
-                    f"bash {scripts_directory}/PureRef-installer.sh {self.version} {self.destination}",
+                    f"bash {scripts_directory}/PureRef-installer.sh  {destination}  {self.destination}",
                     shell=True,
                     check=False,
                 ).returncode
                 != 0
-        ):
-            raise RuntimeError("Failed to install PureRef")
+            ):
+                raise RuntimeError("Failed to install PureRef")
+
+        else:
+            raise RuntimeError("Failed to download PureRef")
--- plugins/scripts/PureRef-installer.sh
+++ plugins/scripts/PureRef-installer.sh
@@ -1,1 +1,5 @@
-echo "Installing $1 to $2"
+echo "Installing PureRef  $1 $2"
+
+chmod +x /tmp/pureref2.Appimage
+/tmp/pureref2.Appimage --appimage-extract > /dev/null
+mv ./squashfs-root "$2/"
