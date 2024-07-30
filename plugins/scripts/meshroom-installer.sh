49d096e203921be7c914a7f14e80e1e61d41d391
ddesmond <crni.aki@gmail.com>
2024-07-30 20:09:06 +0200

    Meshroom wip

--- plugins/scripts/meshroom-installer.sh
+++ plugins/scripts/meshroom-installer.sh
@@ -2,9 +2,3 @@ chmod +x $1
 
 tar -xzvf /tmp/meshroom.tar.gz -C "$2"
 
-#/tmp/kdenlive.appimage --appimage-extract > /dev/null
-#mv ./squashfs-root "$2/"
-#
-#SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-#cp -v "$SCRIPT_DIR/kdenlive.sh" "$2/"
-#chmod +x "$2/kdenlive.sh"
