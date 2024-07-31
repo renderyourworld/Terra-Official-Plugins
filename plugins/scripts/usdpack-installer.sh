92798799137da23be8e418a7b7a7369da3c1c05a
ddesmond <crni.aki@gmail.com>
2024-07-31 06:19:18 +0200

    Add USD v24 py3.10 to Terra -wip

--- plugins/scripts/usdpack-installer.sh
+++ plugins/scripts/usdpack-installer.sh
@@ -1,5 +1,5 @@
-wget -O /tmp/usdpack.zip "$1" -q --show-progress
-wget -O /tmp/kitchen_scene.zip https://s3.eu-central-1.wasabisys.com/juno-deps/Kitchen_set.zip
+wget -q -O /tmp/usdpack.zip "$1"
+wget -q -O /tmp/kitchen_scene.zip https://s3.eu-central-1.wasabisys.com/juno-deps/Kitchen_set.zip
 chmod +x /tmp/usdpack.zip /tmp/kitchen_scene.zip
 unzip /tmp/usdpack.zip -d $2
 unzip /tmp/kitchen_scene.zip -d $2/demo_kitchen_scene
