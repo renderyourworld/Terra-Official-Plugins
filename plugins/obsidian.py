47e5d85dd8f3af0f73ed98a7066558a5ffb31c22
ddesmond <crni.aki@gmail.com>
2024-08-07 06:18:07 +0200

    add obsidian.md to terra

---
+++ plugins/obsidian.py
@@ -0,0 +1,61 @@
+"""
+Installer for obsidian on linux systems.
+"""
+
+# std
+import os
+from subprocess import run
+
+# 3rd
+from terra import Plugin
+
+
+class ObsidianInstaller(Plugin):
+    """
+    Xnview installer plugin.
+    """
+    _alias_ = "Obsidian Installer"
+    icon = "https://github.com/juno-fx/Terra-Official-Plugins/blob/pack-additional-software/plugins/assets/obsidian.png?raw=true"
+    description = "Obsidian.md - md file editor, note taking app."
+    category = "Media and Entertainment"
+    tags = ["obsidian", "editor", "media", "editorial", "kde"]
+    fields = [
+        Plugin.field("url", "Download URL", required=False),
+        Plugin.field("destination", "Destination directory", required=True),
+    ]
+
+    def preflight(self, *args, **kwargs) -> bool:
+        """
+        Check if the target directory exists and validate the arguments passed.
+        """
+        # store on instance
+        self.download_url = kwargs.get(
+            "url",
+            "https://github.com/obsidianmd/obsidian-releases/releases/download/v1.6.7/Obsidian-1.6.7-arm64.AppImage",
+        )
+        self.destination = kwargs.get("destination")
+
+        # validate
+        if not self.destination:
+            raise ValueError("No destination directory provided")
+
+        if not self.destination.endswith("/"):
+            self.destination += "/"
+
+        os.makedirs(self.destination, exist_ok=True)
+
+    def install(self, *args, **kwargs) -> None:
+        """
+        Download and unpack the appimage to the destination directory.
+        """
+        scripts_directory = os.path.abspath(f"{__file__}/../scripts")
+        self.logger.info(f"Loading scripts from {scripts_directory}")
+        if (
+                run(
+                    f"bash {scripts_directory}/obsidian-installer.sh {self.download_url} {self.destination}",
+                    shell=True,
+                    check=False,
+                ).returncode
+                != 0
+        ):
+            raise RuntimeError("Failed to install obsidian")
---
+++ plugins/scripts/obsidian-installer.sh
@@ -0,0 +1,21 @@
+wget -q -O /tmp/obsidian.appimage "$1"
+chmod +x /tmp/obsidian.appimage
+/tmp/obsidian.appimage --appimage-extract > /dev/null
+mv ./squashfs-root "$2/"
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cp -v "$SCRIPT_DIR/obsidian.sh" "$2/"
+chmod +x "$2/obsidian.sh"
+
+# app icon setup
+cp "../assets/obsidian.png" "$2/obsidian.png"
+
+# desktop file setup
+echo "
+[Desktop Entry]
+Name=XnView
+Exec=/bin/bash -x $2/obsidian.sh
+Terminal=true
+Type=Application
+Categories=Apps
+Icon={app_icon}" > "$2/obsidian.desktop"
