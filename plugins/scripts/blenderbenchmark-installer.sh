a15858e035f6ecdd7fa91468d09583e72f9015d3
ddesmond <crni.aki@gmail.com>
2024-08-11 09:06:07 +0200

    Add Blender Benchmark With Classroom example to Terra

--- plugins/scripts/blenderbenchmark-installer.sh
+++ plugins/scripts/blenderbenchmark-installer.sh
@@ -2,23 +2,23 @@ wget -q -O /tmp/blender-benchmark-script.tar.gz $1
 wget -q -O /tmp/classroom.tar.bz2 https://ftp.nluug.nl/pub/graphics/blender/release/BlenderBenchmark2.0/scenes/classroom.tar.bz2
 mkdir $2
 chmod +x /tmp/blender-benchmark-script.tar.gz
-tar -xzvf /tmp/blender-benchmark-script.tar.gz -C "$2"
+tar -xzvf /tmp/blender-benchmark-script.tar.gz -C "$2" > /dev/null
 
 chmod +x /tmp/classroom.tar.bz2
-tar -xvjf /tmp/classroom.tar.bz2 -C "$2"classroom
+tar -xvjf /tmp/classroom.tar.bz2 -C "$2" > /dev/null
 
-#
-echo '
-/apps/blender/latest --background \
+ls "$2"classroom
+ls "$2"blender-benchmark-script-2.0.0
+
+echo "/apps/blender/latest --background \
   --factory-startup \
   -noaudio \
   --debug-cycles \
   --enable-autoexec \
   --engine \
   CYCLES \
-  /"$2"clasroom/main.blend \
+  ${2}clasroom/main.blend \
   --python \
-  "$2"2blender-benchmark-script-2.0.0main.py \
+  $2blender-benchmark-script-2.0.0/main.py \
   -- \
-  --device-type CPU
-' > "$2"blenderbenchmark.sh
+  --device-type CPU" > "$2"blenderbenchmark.sh
