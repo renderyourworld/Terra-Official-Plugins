eecc426fdb0b92ea6827ce4cb9f792696b9e6000
ddesmond <crni.aki@gmail.com>
2024-08-11 08:21:05 +0200

    benchmark wip

---
+++ plugins/scripts/blenderbenchmark-installer.sh
@@ -0,0 +1,24 @@
+wget -q -O /tmp/blender-benchmark-script.tar.gz $1
+wget -q -O /tmp/classroom.tar.bz2 https://ftp.nluug.nl/pub/graphics/blender/release/BlenderBenchmark2.0/scenes/classroom.tar.bz2
+mkdir $2
+chmod +x /tmp/blender-benchmark-script.tar.gz
+tar -xzvf /tmp/blender-benchmark-script.tar.gz -C "$2"
+
+chmod +x /tmp/classroom.tar.bz2
+tar -xvjf /tmp/classroom.tar.bz2 -C "$2"classroom
+
+#
+echo '
+/apps/blender/latest --background \
+  --factory-startup \
+  -noaudio \
+  --debug-cycles \
+  --enable-autoexec \
+  --engine \
+  CYCLES \
+  /"$2"clasroom/clasroom.blend \
+  --python \
+  main.py \
+  -- \
+  --device-type CPU
+' > "$2"blenderbenchmark.sh
