a9af7bafa2f3aa3b3d30277ca3c1b83f8219d58e
ddesmond <crni.aki@gmail.com>
2024-08-11 09:45:12 +0200

    template app for easier building

---
+++ template/templateapp/makeapp.sh
@@ -0,0 +1,10 @@
+echo "Creating app: $1 from app template"
+cd template/templateapp
+cp templateapp.py ../../plugins/$1.py
+sed -i "s/templateapp/$1/g" ../../plugins/$1.py
+cp templateapp-installer.sh ../../plugins/scripts/$1-installer.sh
+sed -i "s/templateapp/$1/g" ../../plugins/scripts/$1-installer.sh
+cp test_templateapp.py ../../tests/test_$1.py
+sed -i "s/templateapp/$1/g" ../../tests/test_$1.py
+
+echo "App $1 created and seeded with templateapp files"
---
+++ template/templateapp/templateapp.py
@@ -0,0 +1,62 @@
+"""
+Installer for templateapp on linux systems.
+"""
+
+# std
+import os
+from subprocess import run
+
+# 3rd
+from terra import Plugin
+
+
+class TemplateappInstaller(Plugin):
+    """
+    Kdenlive installer plugin.
+    """
+
+    _alias_ = "templateapp Installer"
+    icon = "https://kdenlive.org/wp-content/uploads/2022/01/kdenlive-logo-blank-500px.png"
+    description = "templateapp"
+    category = "Media and Entertainment"
+    tags = ["templateapp", "editor", "media", "editorial", "kde"]
+    fields = [
+        Plugin.field("url", "Download URL", required=False),
+        Plugin.field("destination", "Destination directory", required=True),
+    ]
+
+    def preflight(self, *args, **kwargs) -> bool:
+        """
+        Check if the target directory exists and validate the arguments passed.
+        """
+        # store on instance
+        self.download_url = kwargs.get(
+            "url",
+            "templateapp",
+        )
+        self.destination = kwargs.get("destination")
+
+        # validate
+        if not self.destination:
+            raise ValueError("No destination directory provided")
+
+        if not self.destination.endswith("/"):
+            self.destination += "/"
+
+        os.makedirs(self.destination, exist_ok=True)
+
+    def install(self, *args, **kwargs) -> None:
+        """
+        Download and unpack the appimage to the destination directory.
+        """
+        scripts_directory = os.path.abspath(f"{__file__}/../scripts")
+        self.logger.info(f"Loading scripts from {scripts_directory}")
+        if (
+            run(
+                f"bash {scripts_directory}/templateapp-installer.sh {self.download_url} {self.destination}",
+                shell=True,
+                check=False,
+            ).returncode
+            != 0
+        ):
+            raise RuntimeError("Failed to install templateapp")
