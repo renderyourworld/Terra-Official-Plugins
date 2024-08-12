af0dc3024a3000923a0799046395bdd03bc9a6f1
ddesmond <crni.aki@gmail.com>
2024-08-12 15:04:48 +0200

    resolve wip in terra

---
+++ tests/test_resolve.py
@@ -0,0 +1,20 @@
+"""
+Tests for Resolve
+"""
+from os import listdir
+from terra.loaders import plugins
+
+
+def test_resolve():
+    """
+    Test Resolve installer.
+    """
+    handler = plugins()
+    plugin = handler.get_plugin('plugin', 'Resolve Installer')
+    assert plugin is not None
+    handler.run_plugin(
+        'plugin',
+        'Resolve Installer',
+        allow_failure=False,
+        destination='/apps/resolve'
+    )
