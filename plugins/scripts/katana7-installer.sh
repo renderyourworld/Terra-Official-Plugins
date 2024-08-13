9f8bf9c5a383c84749256cc7af69a43327e82cde
ddesmond <crni.aki@gmail.com>
2024-08-13 06:26:29 +0200

    Wip katana 7 support in Terra

--- plugins/scripts/katana7-installer.sh
+++ plugins/scripts/katana7-installer.sh
@@ -1,4 +1,6 @@
 wget -q -O /tmp/Katana7.0v3-linux-x86-release-64.tgz "$1"
 chmod +x /tmp/Katana7.0v3-linux-x86-release-64.tgz
-tar -xvf /tmp/Katana7.0v3-linux-x86-release-64.tgz -C "$2/"
+tar -xvf /tmp/Katana7.0v3-linux-x86-release-64.tgz -C "$2/installler"
+
+"$2"/installler/install.sh --accept-foundry-eula
 
