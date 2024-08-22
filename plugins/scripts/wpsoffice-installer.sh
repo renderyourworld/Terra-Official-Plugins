dbf7d1f776f8269c41fba9b2a52a4c7ed24771a4
ddesmond <crni.aki@gmail.com>
2024-08-22 07:25:45 +0200

    Update WPS Office missing fonts

--- plugins/scripts/wpsoffice-installer.sh
+++ plugins/scripts/wpsoffice-installer.sh
@@ -6,6 +6,18 @@ mv ./squashfs-root "$2/"
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 chmod -R 777 "$2/"
+
+echo "Installing fonts"
+mkdir -p /tmp/getfonts
+cd /tmp/getfonts
+git clone https://github.com/iykrichie/wps-office-19-missing-fonts-on-Linux.git
+cd "wps-office-19-missing-fonts-on-Linux"
+ls -la
+mkdir -p $2/wps-fonts
+mv WEBDINGS.TTF WEBDINGS.ttf
+cp *.ttf $2/wps-fonts
+chmod -R 777 $2/wps-fonts
+
 # app icon setup
 cd $SCRIPT_DIR
 cp ../assets/wpsoffice.png "$2"/wpsoffice.png
