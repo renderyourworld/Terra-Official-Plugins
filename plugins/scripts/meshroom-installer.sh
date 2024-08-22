ed39b10e3cc436e8b9fcb6b49f295e1284a6559e
ddesmond <crni.aki@gmail.com>
2024-08-22 06:17:12 +0200

    Update Meshroom

--- plugins/scripts/meshroom-installer.sh
+++ plugins/scripts/meshroom-installer.sh
@@ -3,7 +3,9 @@ wget -q -O /tmp/meshroom.tar.gz "$1"
 chmod +x /tmp/meshroom.tar.gz
 tar xf /tmp/meshroom.tar.gz -C $2
 chmod -R 777 "$2/"
+
 ls $2
+
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # app icon setup
