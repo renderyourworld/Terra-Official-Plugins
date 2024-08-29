0226a193480ac177b85b08c14b3bea2f6f0a1b8f
ddesmond <crni.aki@gmail.com>
2024-08-29 08:32:22 +0200

    Pycharm patch - update

--- plugins/scripts/pycharm-installer.sh
+++ plugins/scripts/pycharm-installer.sh
@@ -1,22 +1,20 @@
 echo "Installing pycharm-community-$1"
+echo "Downloading pycharm-community-$1"
 
 wget -q -O /tmp/pycharm-community-$1.tar.gz "https://download.jetbrains.com/python/pycharm-community-$1.tar.gz"
 chmod +x /tmp/pycharm-community-$1.tar.gz
 
+ls /tmp/
 mkdir -p $2
-chmod -R 777 $2
+chmod -R 777 $2/
 
 echo "Extracting pycharm-community-$1"
-tar xzvf /tmp/pycharm-community-$1.tar.gz -C "$2"/
-chmod -R 777 "$2"/pycharm-community-$1
+tar xzvf /tmp/pycharm-community-$1.tar.gz -C $2/
 
-chmod -R 777 "$2"/pycharm-community-$1
-chmod -R 777 "$2"
+chmod -R 777 $2/pycharm-community-$1
+chmod -R 777 $2
 
-ln -sfv "$2"/pycharm-community-$1/bin/pycharm.sh "$2"/latest
-
-ls -l "$2"/
-ls -l "$2"/pycharm-community-$1
+ln -sfv "$2"/pycharm-community-$1/bin/pycharm.sh $2/latest
 
 # app icon setup
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
@@ -24,9 +22,10 @@ cd $SCRIPT_DIR
 cp "../assets/pycharm.png" "$2/pycharm.png"
 echo "Adding desktop file"
 chmod +X create_desktop_file.py
-python3 create_desktop_file.py --app_name="Pycharm" --version=$1 --latest_path="$2/latest" --categories="pycharm, ide" --destination="$2" --icon="$2"/pycharm.png
+python3 create_desktop_file.py --app_name="Pycharm" --version="Juno" --latest_path="$2/latest" --categories="pycharm, ide" --destination="$2" --icon="$2"/pycharm.png
 echo "Desktop file created."
 
-chmod -R 777 "$2"
+chmod -R 777 "$2" > /dev/null
 
 cat $2/*.desktop
+
