aff2402d45b73169f7b0c061ba29bfee69675de5
ddesmond <crni.aki@gmail.com>
2024-09-03 05:02:20 +0200

    Stardust test

---
+++ tests/test_stardust.py
@@ -0,0 +1,20 @@
+"""
+Tests for stardust
+"""
+from os import listdir
+from terra.loaders import plugins
+
+
+def test_stardust():
+    """
+    Test stardust installer.
+    """
+    handler = plugins()
+    plugin = handler.get_plugin('plugin', 'Stardust')
+    assert plugin is not None
+    handler.run_plugin(
+        'plugin',
+        'Stardust',
+        allow_failure=False,
+        pat="PERSONAL ACCES TOKEN HERE",
+    )
