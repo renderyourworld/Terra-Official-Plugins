51d63452f2ad06154375d0dc623466b8e60a400f
ddesmond <crni.aki@gmail.com>
2024-09-03 07:20:07 +0200

    Add Steam wip

---
+++ plugins/steam.py
@@ -0,0 +1,56 @@
+"""
+Installer for steam on linux systems.
+"""
+
+# std
+import os
+from subprocess import run
+from pathlib import Path
+
+# 3rd
+from terra import Plugin
+
+
+class SteamInstaller(Plugin):
+    """
+    steam installer plugin.
+    """
+
+    _alias_ = "Steam Client"
+    icon = "https://github.com/juno-fx/Terra-Official-Plugins/blob/main/plugins/assets/steam.png?raw=true"
+    description = "Steam Client installer"
+    category = "Media and Entertainment"
+    tags = ["steam", "client", "games"]
+    fields = [
+    ]
+
+    def preflight(self, *args, **kwargs) -> bool:
+        """
+        Check if the target directory exists and validate the arguments passed.
+        """
+        # store on instance
+        self.download_url = "https://github.com/ddesmond/Steam-appimage/releases/download/continuous/Steam-1.0.0.81-2-3-x86_64.AppImage"
+        self.destination = Path(kwargs.get("destination")).as_posix()
+
+
+        # validate
+        if not self.destination:
+            raise ValueError("No destination directory provided")
+
+        os.makedirs(self.destination, exist_ok=True)
+
+    def install(self, *args, **kwargs) -> None:
+        """
+        Download and unpack the appimage to the destination directory.
+        """
+        scripts_directory = os.path.abspath(f"{__file__}/../scripts")
+        self.logger.info(f"Loading scripts from {scripts_directory}")
+        if (
+            run(
+                f"bash {scripts_directory}/steam-installer.sh {self.download_url} {self.destination}",
+                shell=True,
+                check=False,
+            ).returncode
+            != 0
+        ):
+            raise RuntimeError("Failed to install steam")
