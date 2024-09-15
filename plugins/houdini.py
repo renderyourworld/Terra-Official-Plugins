56cb92310d9b32aa788042678ecfef0d151d2585
ddesmond <crni.aki@gmail.com>
2024-09-15 08:38:16 +0200

    houdini patch, wip

--- plugins/houdini.py
+++ plugins/houdini.py
@@ -52,7 +52,7 @@ class HoudiniInstaller(Plugin):
         self.client_id = kwargs.get("client_id")
         self.client_secret = kwargs.get("client_secret")
         self.destination = Path(kwargs.get("destination")).as_posix()
-
+        self.install_volume='terra-test-claim',
         assert self.client_secret
         assert self.client_id
         assert self.version
@@ -62,8 +62,6 @@ class HoudiniInstaller(Plugin):
 
         assert self.version
         assert self.build
-
-
 
         # validate
         if not self.destination:
