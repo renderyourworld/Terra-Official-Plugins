a84bd7d5ded1582dde294cad4e3d0af6adc7e6d3
ddesmond <crni.aki@gmail.com>
2024-09-19 08:55:02 +0200

    template update

--- template/templateapp/templateapp-installer.sh
+++ template/templateapp/templateapp-installer.sh
@@ -1,22 +1,22 @@
 # template for appimage
-#echo "Installing firefox..."
-#wget -q -O /tmp/firefox.appimage "$1"
-#chmod +x /tmp/firefox.appimage
+#echo "Installing templateapp..."
+#wget -q -O /tmp/templateapp.appimage "$1"
+#chmod +x /tmp/templateapp.appimage
 #
-#echo "Extracting firefox..."
-#/tmp/firefox.appimage --appimage-extract > /dev/null
+#echo "Extracting templateapp..."
+#/tmp/templateapp.appimage --appimage-extract > /dev/null
 #mv ./squashfs-root "$2/"
 #SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-#cp -v "$SCRIPT_DIR/firefox.sh" "$2/"
-#sed -i "s@ROOT_APP@$2@g" "$2/firefox.sh"
-#chmod +x "$2/firefox.sh"
+#cp -v "$SCRIPT_DIR/templateapp.sh" "$2/"
+#sed -i "s@ROOT_APP@$2@g" "$2/templateapp.sh"
+#chmod +x "$2/templateapp.sh"
 #chmod -R 777 "$2/"
 ## app icon setup
 #cd $SCRIPT_DIR
-#cp "../assets/firefox.png" "$2/firefox.png"
+#cp "../assets/templateapp.png" "$2/templateapp.png"
 #echo "Adding desktop file"
 #chmod +X create_desktop_file.py
-#python3 create_desktop_file.py --app_name="Firefox" --version="30.0" --latest_path="$2"/firefox.sh --categories="firefox, web" --destination="$2" --icon="$2"/firefox.png --terminal="False"
+#python3 create_desktop_file.py --app_name="templateapp" --version="30.0" --latest_path="$2"/templateapp.sh --categories="templateapp" --destination="$2" --icon="$2"/templateapp.png --terminal="False"
 #echo "Desktop file created."
 #chmod -R 777 "$2/"
 #cat $2/*.desktop
