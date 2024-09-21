9bfe77ec2ba89e8a95884a66f1ff5fa2acc6df75
ddesmond <crni.aki@gmail.com>
2024-09-21 17:40:16 +0200

    Add Unreal WIP

---
+++ tests/test_unreal.py
@@ -0,0 +1,20 @@
+"""
+Tests for unreal
+"""
+from os import listdir
+from terra.loaders import plugins
+
+
+def test_unreal():
+    """
+    Test unreal installer.
+    """
+    handler = plugins()
+    plugin = handler.get_plugin('plugin', 'unreal Installer')
+    assert plugin is not None
+    handler.run_plugin(
+        'plugin',
+        'unreal Installer',
+        allow_failure=False,
+        destination='/apps/unreal'
+    )
