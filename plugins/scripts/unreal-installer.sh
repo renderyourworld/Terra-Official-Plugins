536af0c90b896366e8806c68a8a9453088ce7c43
ddesmond <crni.aki@gmail.com>
2024-09-21 17:44:37 +0200

    Unreal wip

--- plugins/scripts/unreal-installer.sh
+++ plugins/scripts/unreal-installer.sh
@@ -1,11 +1,15 @@
  template for appimage
-echo "Installing unreal..."
+echo "Installing Unreal... Long process..."
+cd /tmp
+unreal_version=$4
 wget -q -O /tmp/unreal.zip "$1"
 chmod +x /tmp/unreal.zip
-
+wget -q -O /tmp/unreal_bridge.zip "$1"
+chmod +x /tmp/unreal_bridge.zip
 mkdir -p $2
 echo "Extracting unreal..."
 unzip /tmp/unreal.zip -C $2/
+#unzip /tmp/unreal_bridge.zip -C $2/
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cp -v "$SCRIPT_DIR/unreal.sh" "$2/"
@@ -18,7 +22,7 @@ cd $SCRIPT_DIR
 cp "../assets/unreal.png" "$2/unreal.png"
 echo "Adding desktop file"
 chmod +X create_desktop_file.py
-python3 create_desktop_file.py --app_name="unreal" --version="5.4.4" --latest_path="$2"/unreal.sh --categories="unreal" --destination="$2" --icon="$2"/unreal.png --terminal="False"
+python3 create_desktop_file.py --app_name="unreal" --version=$unreal_version --latest_path="$2"/unreal.sh --categories="unreal" --destination="$2" --icon="$2"/unreal.png --terminal="False"
 echo "Desktop file created."
 chmod -R 777 "$2/"
 cat $2/*.desktop
--- plugins/unreal.py
+++ plugins/unreal.py
@@ -15,7 +15,7 @@ class unrealInstaller(Plugin):
     """
     unreal installer plugin.
     """
-
+    _version_ = "0.1.0"
     _alias_ = "unreal Installer"
     icon = "https://github.com/juno-fx/Terra-Official-Plugins/blob/main/plugins/assets/missing.png?raw=true"
     description = "unreal"
@@ -25,6 +25,7 @@ class unrealInstaller(Plugin):
         Plugin.field("url", "Download URL", required=False),
         Plugin.field("bridge_url", "Download Bridge URL", required=False),
         Plugin.field("destination", "Destination directory", required=True),
+        Plugin.field("unreal_version", "Unreal version, 5.4.0, 5.4.4", required=True),
     ]
 
     def preflight(self, *args, **kwargs) -> bool:
@@ -41,6 +42,10 @@ class unrealInstaller(Plugin):
             "https://ucs-blob-store.s3-accelerate.amazonaws.com/blobs/16/11/44fe-b519-4e8f-a303-ffa2b0a8f668?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIA2SBBZFECCYQWRK6G%2F20240921%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240921T145352Z&X-Amz-Expires=3600&X-Amz-Signature=f3bacace6962d793519c0cfa3a20c9261b111acd0b8a14643cf1ffa897f93bfb&X-Amz-SignedHeaders=host&response-content-disposition=inline%3Bfilename%3D%22file.zip%22%3Bfilename%2A%3DUTF-8%27%27Linux_Bridge_5.4.0_2024.0.1.zip&x-id=GetObject"
         )
         self.destination = Path(kwargs.get("destination")).as_posix()
+        self.unreal_version = kwargs.get(
+            "unreal_version",
+            "5.4.4"
+        )
 
 
         # validate
@@ -57,7 +62,7 @@ class unrealInstaller(Plugin):
         self.logger.info(f"Loading scripts from {scripts_directory}")
         if (
             run(
-                f"bash {scripts_directory}/unreal-installer.sh {self.download_url} {self.destination}  {self.download_bridge_url}",
+                f"bash {scripts_directory}/unreal-installer.sh {self.download_url} {self.destination}  {self.download_bridge_url} {self.unreal_version}",
                 shell=True,
                 check=False,
             ).returncode
