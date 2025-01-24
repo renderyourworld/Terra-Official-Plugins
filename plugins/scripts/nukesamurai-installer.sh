477dbb7e72c4a95b55a3672db2590a16c9ff83db
ddesmond <crni.aki@gmail.com>
2025-01-24 21:15:33 +0100

    pyenv py3.10 dependency

--- plugins/scripts/nukesamurai-installer.sh
+++ plugins/scripts/nukesamurai-installer.sh
@@ -2,7 +2,22 @@
 # requires python3.10
 # requires git
 
-apt install python3.10 git -y
+apt update -y &&  apt upgrade -y && apt update -y
+apt install build-essential libssl-dev zlib1g-dev \
+libbz2-dev libreadline-dev libsqlite3-dev curl \
+libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev -y
+
+curl https://pyenv.run | bash
+export PYENV_ROOT="$HOME/.pyenv"
+command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+
+pyenv install 3.10.10
+pyenv local 3.10
+echo "Python 3.10 installed ?"
+python3 --version
+
 
 echo "Installing NukeSamurai to /apps/nuke_samurai"
 export BUILDER=/tmp/nuke_samurai
@@ -13,22 +28,26 @@ cd $BUILDER
 git clone https://github.com/Theo-SAMINADIN-td/NukeSamurai.git
 cd $BUILDER/NukeSamurai
 
-python3.10 -m venv $BUILDER/NukeSamurai/venv
-source $BUILDER/NukeSamurai/venv/bin/activate
+echo "Installing NukeSamurai PIP deps"
+python3 -m venv $BUILDER/NukeSamurai/venv
+source venv/bin/activate
+echo "Check pip / venv?"
+pip --version
 
-# pull pip deps
-pip install torch==2.3.1+cu118 torchvision==0.18.1 --extra-index-url https://download.pytorch.org/whl/cu118
+#
+## pull pip deps
+pip install -i https://pypi.org/simple torch==2.3.1+cu118 torchvision==0.18.1 --extra-index-url https://download.pytorch.org/whl/cu118
 cd $BUILDER/NukeSamurai/sam2_repo
-pip install -e .
-pip install -e ".[notebooks]"
-pip install matplotlib==3.7 tikzplotlib jpeg4py opencv-python lmdb pandas scipy loguru ninja
+pip install -i https://pypi.org/simple -e .
+pip install -i https://pypi.org/simple -e ".[notebooks]"
+pip install -i https://pypi.org/simple matplotlib==3.7 tikzplotlib jpeg4py opencv-python lmdb pandas scipy loguru ninja
 
-# download checkpoints
-cd checkpoints && \
-./download_ckpts.sh && \
-cd ..
-
-mv $BUILDER/NukeSamurai $1
-echo "NukeSamurai installed to $1"
-
-
+## download checkpoints
+#cd checkpoints && \
+#./download_ckpts.sh && \
+#cd ..
+#
+#mv $BUILDER/NukeSamurai $1
+#echo "NukeSamurai installed to $1"
+#
+#
