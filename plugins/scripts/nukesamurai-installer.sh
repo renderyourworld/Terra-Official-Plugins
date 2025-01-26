64b993753b1eb081cd4bb35ccbef22d74edbfd1a
ddesmond <crni.aki@gmail.com>
2025-01-26 07:00:52 +0100

    add shortcut and env

--- plugins/scripts/nukesamurai-installer.sh
+++ plugins/scripts/nukesamurai-installer.sh
@@ -58,4 +58,11 @@ chmod -R 777 $1
 
 echo "NukeSamurai installed to $1"
 
+# app icon setup
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd $SCRIPT_DIR
+cp "../assets/nuke.png" "$2/nuke.png"
+echo "Adding desktop file"
+chmod +X create_desktop_file.py
+python3 create_desktop_file.py --app_name="NukeSamurai" --version=$1 --latest_path="$1"/nukesamura-start.sh --categories="nuke, samurai" --destination="$1" --icon="$1"/nuke.png --terminal="False"
