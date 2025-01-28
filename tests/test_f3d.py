b7105a117241d21f9de1dfccae8858a147c8df28
ddesmond <crni.aki@gmail.com>
2025-01-28 16:36:56 +0100

    add more code

--- tests/test_f3d.py
+++ tests/test_f3d.py
@@ -14,7 +14,6 @@ def test_f3d():
     assert plugin is not None
     assert plugin._version_ is not None
     handler.run_plugin(
-        'plugin',
         'F3d Installer',
         allow_failure=False,
         destination='/apps/f3d'
