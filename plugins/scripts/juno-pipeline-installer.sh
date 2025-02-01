57df544e6ec01fcbf76d21e39aa83d75b09f4686
dwalters <danjwalters@gmail.com>
2025-02-01 09:02:17 -0500

    adjusted based on feedback

--- plugins/scripts/juno-pipeline-installer.sh
+++ plugins/scripts/juno-pipeline-installer.sh
@@ -7,6 +7,3 @@ echo "Adding desktop file"
 chmod +X create_desktop_file.py
 python3 create_desktop_file.py --app_name="Juno" --version="Nuke" --latest_path="juno nuke" --categories="nuke, compositing" --destination="$1" --icon="$1"/nuke.png --terminal="True"
 echo "Desktop file created."
-
-cat $1/*.desktop
-
