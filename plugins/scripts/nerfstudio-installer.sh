1d198b6694967a750da8d00b35cadcf4141f46cb
ddesmond <crni.aki@gmail.com>
2025-02-02 09:05:22 +0100

    install updates

--- plugins/scripts/nerfstudio-installer.sh
+++ plugins/scripts/nerfstudio-installer.sh
@@ -3,9 +3,17 @@ echo "Installing NerfStudio..."
 export PIP_INDEX_URL=https://pypi.org/simple
 export UV_LINK_MODE=copy
 
-# we need to rebuild python
+# apt
 apt update -y &&  apt upgrade -y && apt update -y
+apt-get install build-essential git -y
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb
+dpkg -i cuda-keyring_1.1-1_all.deb
+apt-get update -y
+apt-get -y install cuda-toolkit-12-4
 
+# needs tkinter in polaris aswell
+apt install python3-tk -y
+apt install colmap -y
 
 uv python install 3.10.12
 
@@ -19,7 +27,14 @@ echo "Installing NerfStudio PIP deps"
 source $1/venv/bin/activate
 uv pip install --upgrade pip
 
+cd $1
+git clone --recursive https://github.com/nvlabs/tiny-cuda-nn
+cd tiny-cuda-nn/bindings/torch
+python3 setup.py install
+
+# /home/des/.cache/torch/ link
 echo "Install NerfStudio"
+cd $1
 uv pip install nerfstudio
 
 echo "Install Gradio"
@@ -28,6 +43,8 @@ uv pip install gradio
 echo "Clone WebUI install"
 cd $1
 git clone https://github.com/nerfstudio-project/nerfstudio-webui.git
+
+
 
 # app icon setup
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
