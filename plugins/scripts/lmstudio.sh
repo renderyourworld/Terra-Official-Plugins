f7005c3845c578b4cdcdfe5aab7669296e83b62d
ddesmond <crni.aki@gmail.com>
2025-02-03 08:04:07 +0100

    patch models symlink

--- plugins/scripts/lmstudio.sh
+++ plugins/scripts/lmstudio.sh
@@ -5,8 +5,7 @@ echo "Checking if .lmstudio directory exists"
 sleep 1
 if [ ! -d "$HOME/.lmstudio" ]; then
   mkdir -p $HOME/.lmstudio
-  mkdir -p $HOME/.lmstudio/models
-  ln -s ROOT_APP/cache/models $HOME/.lmstudio
+  ln -s ROOT_APP/cache/models $HOME/.lmstudio/models
   echo "Created .lmstudio directory"
   echo "Linked .lmstudio/models directory from apps."
   sleep 1
