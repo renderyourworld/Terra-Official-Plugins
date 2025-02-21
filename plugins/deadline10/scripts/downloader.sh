389812b321449831c06fa18ab377c35b5ff53c2c
aldmbmtl <alex.e.hatfield@gmail.com>
2025-02-20 19:57:23 -0500

    Adjusting download target location

--- plugins/deadline10/scripts/downloader.sh
+++ plugins/deadline10/scripts/downloader.sh
@@ -1,10 +1,11 @@
 echo "Installing $1"
 
+# install wget
 apt update
 apt install -y wget
 
 echo "Downloading Deadline installer"
-installers=/apps/tmp/deadline10_installers
+installers=/mnt/tmp/deadline10_installers
 mkdir -p $installers
 
 if [ "$DEV_APPS_DEBUG" = true ]
@@ -12,13 +13,16 @@ then
   echo "Dev Apps Debug is enabled - deadline copied with docker"
 else
   # download the installer
-  wget -q -O /tmp/Deadline-10.3.2.1-linux-installers.tar "$1"
+  wget -q -O /mnt/tmp/Deadline-10.3.2.1-linux-installers.tar "$1"
 fi
+
 echo "Downloaded Deadline installer."
+
 # permissions
-chmod +x /tmp/Deadline-10.3.2.1-linux-installers.tar
+chmod +x /mnt/tmp/Deadline-10.3.2.1-linux-installers.tar
+
 # extract the installer
-tar -xvf /tmp/Deadline-10.3.2.1-linux-installers.tar -C $installers
+tar -xvf /mnt/tmp/Deadline-10.3.2.1-linux-installers.tar -C $installers
 echo "Deadline installer files extracted to $installers"
 
 
