8489d22d59268259a9cc3d3660efa9118488754e
aldmbmtl <alex.e.hatfield@gmail.com>
2025-02-20 19:38:56 -0500

    Testing out packager

--- plugins/deadline10/scripts/entrypoint.sh
+++ plugins/deadline10/scripts/entrypoint.sh
@@ -1,9 +1,7 @@
-if [ -z "$SETUP_FILES" ]; then
+if [ -n "${SETUP_FILES}" ]; then
     /terra/scripts/setup_files.sh "$DESTINATION" "$CUSTOM_PATH"
 fi
 
-if [ -z "$DOWNLOAD" ]; then
-    /terra/scripts/download.sh "$URL" "$DESTINATION"
+if [ -n "${DOWNLOAD}" ]; then
+    /terra/scripts/downloader.sh "$URL" "$DESTINATION"
 fi
-
-sleep infinity
