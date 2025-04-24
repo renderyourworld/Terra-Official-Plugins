27573178d64d0113957d6423512dcccf2b18a573
aldmbmtl <alex.e.hatfield@gmail.com>
2025-04-24 14:20:54 -0400

    Testing

--- plugins/nuke/scripts/installer.sh
+++ plugins/nuke/scripts/installer.sh
@@ -2,19 +2,18 @@ set -e
 
 echo "Installing $VERSION"
 echo "Destination $DESTINATION"
-#
-#curl -LJO -o nuke.tgz "https://www.foundry.com/products/download_product?file=$1-linux-x86_64.tgz"
-#mv -v "download_product" "/tmp/$VERSION.tgz"
-#
-#executable="$(echo "$1" | cut -d'v' -f1)"
-#echo "Executable: $executable"
-#
-#tar -xvf "/tmp/$1.tgz" -C /tmp/ > /dev/null
-#"/tmp/$1-linux-x86_64.run" --prefix="$2/" --accept-foundry-eula > /dev/null
-#
-#rm -rfv "$2/$1.tgz" "$2/$1-linux-x86_64.run"
-#ln -sfv "$2/$1/$executable" "$2/latest"
-#
+
+curl -LJO -o nuke.tgz "https://www.foundry.com/products/download_product?file=$VERSION-linux-x86_64.tgz"
+mv -v "download_product" "/tmp/$VERSION.tgz"
+
+executable="$(echo "$VERSION" | cut -d'v' -f1)"
+echo "Executable: $executable"
+
+tar -xvf "/tmp/$VERSION.tgz" -C /tmp/
+"/tmp/$1-linux-x86_64.run" --prefix="$DESTINATION/" --accept-foundry-eula
+
+rm -rfv "$DESTINATION/$VERSION.tgz" "$DESTINATION/$VERSION-linux-x86_64.run"
+
 #SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 #
 ## app icon setup
