828239f47bd63c4483d1283f3948444ed76eace9
aldmbmtl <alex.e.hatfield@gmail.com>
2025-04-24 23:09:06 -0400

    Adding in updated desktop file generation

--- plugins/nuke/scripts/installer.sh
+++ plugins/nuke/scripts/installer.sh
@@ -3,14 +3,16 @@ set -e
 apt update
 apt install curl -y
 
+LAUNCH="$DESTINATION/$VERSION/$executable"
+ICON="$DESTINATION/nuke.png"
+executable="$(echo "$VERSION" | cut -d'v' -f1)"
+
 echo "Installing $VERSION"
 echo "Destination $DESTINATION"
+echo "Executable: $executable"
 
 curl -LJO -o nuke.tgz "https://www.foundry.com/products/download_product?file=$VERSION-linux-x86_64.tgz"
 mv -v "download_product" "/tmp/$VERSION.tgz"
-
-executable="$(echo "$VERSION" | cut -d'v' -f1)"
-echo "Executable: $executable"
 
 tar -xvf "/tmp/$VERSION.tgz" -C /tmp/
 "/tmp/$VERSION-linux-x86_64.run" --prefix="$DESTINATION/" --accept-foundry-eula
@@ -19,14 +21,16 @@ rm -rfv "$DESTINATION/$VERSION.tgz" "$DESTINATION/$VERSION-linux-x86_64.run"
 
 # app icon setup
 cp -v ./assets/nuke.png $DESTINATION/
-cp -v ./assets/nuke.desktop $DESTINATION/
 
-LAUNCH="$DESTINATION/$VERSION/$executable"
-ICON="$DESTINATION/nuke.png"
-
-sed -i -e 's/APP-VERSION/$VERSION/g' $DESTINATION/nuke.desktop
-sed -i -e 's/DESTINATION-PATH/$LAUNCH/g' $DESTINATION/nuke.desktop
-sed -i -e 's/ICON-PATH/$ICON/g' $DESTINATION/nuke.desktop
+echo "[Desktop Entry]
+Version=$VERSION
+Name=Nuke $VERSION
+Comment=Nuke compositing software
+Exec=junogl $LAUNCH
+Icon=$ICON
+Terminal=false
+Type=Application
+Categories=X-Polaris" >> $DESTINATION/nuke.desktop
 
 cat $DESTINATION/*.desktop
 
