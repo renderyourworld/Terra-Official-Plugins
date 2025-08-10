0a39912fee728444daaf6703c8f317e65e92dfd2
renderyourworld <renderyourworld@gmail.com>
2025-08-09 23:23:59 -0400

    testing

--- plugins/minecraft/scripts/downloader.sh
+++ plugins/minecraft/scripts/downloader.sh
@@ -18,6 +18,12 @@ tar -xzf "/tmp/Minecraft.tar.gz" -C "/tmp"
 cd "$2"
 ls -la
 
+echo "Checking for old minecraft-launcher directory..."
+if [ -d "minecraft-launcher" ]; then
+    echo "Found existing directory. Removing it and its contents..."
+    rm -rf "minecraft-launcher"
+fi
+
 # Move 'minecraft-launcher' directory to destination directory
 mv -v "/tmp/minecraft-launcher" "$2/"
 
